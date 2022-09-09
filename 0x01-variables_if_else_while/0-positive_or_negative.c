46f60ecd9931ec61796f8e4042736b9148fedd32
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-09 13:07:53 -0700

    C Variable task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - print numbers if positive or negative
+ * main - print positive, zero or negative numbers
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -13,13 +14,13 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d, is positive\n", n);
+		printf("%d is positive\n", n);
 	} else if (n == 0)
 	{
-		printf("%d, is zero\n", n);
+		printf("%d is zero\n", n);
 	} else if (n < 0)
 	{
-		printf("%d, is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 
