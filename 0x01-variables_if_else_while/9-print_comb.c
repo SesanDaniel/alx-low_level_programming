eff6ea536f5d719b0a33ece440646e71a940846c
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-10 10:15:11 -0700

    C Variables task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int j;
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(j);
+		if (j != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
