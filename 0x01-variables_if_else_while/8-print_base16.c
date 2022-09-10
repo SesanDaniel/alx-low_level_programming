a7f4c779a7fd67eea00bd0dcfb972253cfd004f8
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-10 09:56:29 -0700

    C Variables task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char low;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+		for (low = 'A'; low <= 'F'; low++)
+			putchar(low);
+	putchar('\n');
+}
