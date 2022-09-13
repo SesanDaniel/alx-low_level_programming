fdf93a4a8438eda5df5ee0fbcecd86a162661a04
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-13 13:18:46 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - check description
+ * @n: an input number
+ * Description: a function that prints the sign of a number
+ * Return: Nothing
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		value = -1;
+		_putchar('-');
+	}
+	
+	return (value);
+}
