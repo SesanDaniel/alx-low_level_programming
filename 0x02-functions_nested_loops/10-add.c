dcea9d3d3057581dc815845d502f7c5d329c2bc1
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-13 14:39:58 -0700

    task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check description
+ * Description: a function that adds two integers and returns the result.
+ * Return: Nothing
+ */
+
+int add(int, int)
+{
+	char a, b, num;
+	 num = (a + b);
+
+	_putchar(num);
+}
