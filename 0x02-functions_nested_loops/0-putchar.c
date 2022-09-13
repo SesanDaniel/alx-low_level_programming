50aa40554c21319c98f0d9b727c0d6a4ccc94c02
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-13 11:24:15 -0700

    c functions and nested loops task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	char j[8] = "_putchar";
+	char text[8] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(j[i]);
+		_putchar(text[i]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 	
 	return (0);
 }
