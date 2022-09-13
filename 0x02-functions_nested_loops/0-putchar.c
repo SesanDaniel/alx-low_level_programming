0e5aa88a272c478c882b9c65c33d76a619030d83
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-13 10:35:51 -0700

    C-Functions, nested loops task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
+#include <unistd.h>
+#include "_putchar.h"
 
 /**
  * main - Print _putchar
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	char text[8] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
-		_putchar(text);
+		_putchar(text[i]);
 	_putchar("\n");
 	
 	return (0);
