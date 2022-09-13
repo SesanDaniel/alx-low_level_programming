5ace63e0b1546d730a45f4c436ec936f9bd240ce
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-13 11:55:05 -0700

    c functions and nested loops task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check description
- * Description - A function that prints all alphabets in lowercase
+ * print_alphabet - check description
+ * Description: A function that prints all alphabets in lowercase
  * Return: Always 0 (Success)
  */
 
