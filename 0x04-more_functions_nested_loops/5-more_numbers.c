41567fe218be7c78373a27e271bd7df9e05ae5fe
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-15 16:19:10 -0700

    more functions task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int i,j;
 
-	for (j = 0; j <= 14; j++)
+	for (j = '0'; j <= '14'; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = '0'; i <= '14'; i++)
 		{
 			if (i > 9)
 				_putchar(i / 10 + '0');
