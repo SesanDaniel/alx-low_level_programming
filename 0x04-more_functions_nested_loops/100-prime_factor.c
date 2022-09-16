0820a00b69a22242962251287e37144b4c13792f
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-16 06:38:19 -0700

    more functions advance task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	unsigned long int i = 3, n = 612852475143;
+	unsigned long int i, n = 612852475143;
 
-	for (; i < 12057; i += 2)
+	for (i = 3; i < 12057; i += 2)
 	{
-		while (n % 1 == 0 && n != i)
-			n /= 1;
+		while (n % i == 0 && n != i)
+			n /= i;
 	}
 	printf("%lu\n", n);
 	return (0);
