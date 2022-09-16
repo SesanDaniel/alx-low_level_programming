00ded98d8ea3c555ee4ad86b6d3a0aa8a1647597
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-16 07:28:54 -0700

    debugging task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -16,11 +16,11 @@ int main(void)
 	/**
 	 * while (i < 10)
 	 * {
-	 * 	putchar(i);
+	 * putchar(i);
 	 * }
 	 */
 
-	printf("Infinite loop avoided \\0/\n");
+	printf("Infinite loop avoided! \\0/\n");
 
-	return (0)
+	return (0);
 }
