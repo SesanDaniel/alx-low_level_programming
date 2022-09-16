e7ecbe384d0743585fd7e504ac2eb809878d188c
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-16 06:00:13 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2;)
+	{
+		while (n % 1 == 0 && n != i)
+			n /= 1;
+	}
+	printf("%lu\n", n);
+	return(0);
+}
