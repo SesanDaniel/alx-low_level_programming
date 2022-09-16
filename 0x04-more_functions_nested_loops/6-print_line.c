ae7981d4d12536668cbfd3ee3342cadfb50828da
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-15 17:13:12 -0700

    more functions task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,7 +4,7 @@
  * print_line - check description
  * Description: a function that draws a straight line in the terminal.
  * @n: integer of numbers 
- * Return: nothing
+ * Return: Always 0
  */
 
 void print_line(int n)
