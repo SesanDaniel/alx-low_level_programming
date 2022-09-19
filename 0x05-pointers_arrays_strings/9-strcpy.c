dd889253e3c930278aa953efc125736655883db8
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-19 08:59:10 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,6 @@
-#include "main.h"
-
 /**
- * _strcpy - a function that copies the string 
- * pointed to by src, including the terminating null 
+ * _strcpy - a function that copies the string
+ * pointed to by src, including the terminating null
  * byte (\0), to the buffer pointed to by dest.
  * @dest: Destination pointer for a string
  * @src: Scource pointer to copy from
