fd6a571243f18c51ce933e2aad4bfe2a0207a13e
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-26 09:33:25 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * _strpbrk - a function that searches a string for any of a set of bytes.
  * @s:string
  * @accept: Input character to locate in string
- * Return: a pointer to the byte in s that matches one of the bytes in accept, 
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
  * or NULL if no such byte is found
  */
 
