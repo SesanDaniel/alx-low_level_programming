cada2d4747aa1e292a996a8434e93b4bb25ed0b9
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-26 09:09:12 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: The string to be located
+ * @c: The character of a string to be located
+ * Return: Pointer to position of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (c == *s)
+			return (s);
+		s++;
+	}
+	if (c == *s)
+		return (s);
+	return (NULL);
+}
