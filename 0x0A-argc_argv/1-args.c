7fa857b3f54db72f080b588ef7fe698ec852ac5c
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-30 06:49:17 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A programm that prints the number of rguments passed into it
+ * @argc: Argument count
+ * @argv: Argument vector (Array or arguments)
+ * Return: Always (0) sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+}
