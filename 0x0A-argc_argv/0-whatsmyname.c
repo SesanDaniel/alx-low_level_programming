db6631e1cba30106f260361b0d0b1b5aaa673d5b
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-30 06:33:13 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - a program that prints its name, followed by a new line.
+ * @argc: ARGUMENT COUNTER
+ * @argv: ARGUMENT VECTOR (ARRAY)
  * Return: Always (0) success
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		_putchar("%s\n", argv[i]);
-	}
+	if (argc < 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
