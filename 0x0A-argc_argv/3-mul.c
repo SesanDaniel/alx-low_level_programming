32c31151a174abbd7ebc1addbc483182b0da72e1
SesanDaniel <obisesan.seyid@gmail.com>
2022-09-30 08:52:38 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	for ( i = 0; i < argc; i++)
+	{
+		if (argc > 3)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+			printf("%d\n", mul);
+		}
+		return (0);
+	}
+}
