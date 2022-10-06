06c1655285237c47e21456c4a8f87ce3938ad374
SesanDaniel <obisesan.seyid@gmail.com>
2022-10-06 03:44:15 -0700

    more malloc and free task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: An unsigned integer input
+ * Return: A pointer to newly allocated memory on success and NULL on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This project is more on Malloc and free.
