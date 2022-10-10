3433d912d486591d4621fa37c47f4fb00569cb7b
SesanDaniel <obisesan.seyid@gmail.com>
2022-10-10 10:07:40 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,7 +16,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 	if (d == NULL)
 		return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
